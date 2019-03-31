025162263da016233d7c82f56475bf741354cbc1
gyunt <truth0233@gmail.com>
2019-03-31 17:41:10 +0900

    improve load/save model.

--- baselines/hiro/hiro.py
+++ baselines/hiro/hiro.py
@@ -168,6 +168,7 @@ def learn(*, network, env, total_timesteps, eval_env=None, seed=None, nsteps=128
         save_graph("i:/tmp/")
 
         if load_path is not None:
+            print('Loading from', load_path)
             load_variables(load_path, sess=sess)
 
         high_allvars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, high_model.scope.name)
@@ -279,8 +280,9 @@ def learn(*, network, env, total_timesteps, eval_env=None, seed=None, nsteps=128
                 if MPI is None or MPI.COMM_WORLD.Get_rank() == 0:
                     logger.dumpkvs()
 
-            if save_interval and (update % save_interval == 0 or update == 1) and logger.get_dir() and (
-                MPI is None or MPI.COMM_WORLD.Get_rank() == 0):
+            if update == nupdates or \
+                (save_interval and (update % save_interval == 0 or update == 1) and logger.get_dir() and (
+                    MPI is None or MPI.COMM_WORLD.Get_rank() == 0)):
                 checkdir = os.path.join(logger.get_dir(), 'checkpoints')
                 os.makedirs(checkdir, exist_ok=True)
                 savepath = os.path.join(checkdir, '%.5i' % update)
